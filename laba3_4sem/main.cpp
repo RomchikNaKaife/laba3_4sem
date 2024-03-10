d5ba821cc26a44d6262d6fc3a8fffedf63862150
Roman <roman.spam2004@yaandex.ru>
2024-03-10 23:40:59 +0300

    Initial Commit

---
+++ laba3_4sem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  laba3_4sem
+//
+//  Created by Роман Перепонов on 10.03.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
